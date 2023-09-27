4b3099c798ec08e282209bafa274b8fb8a0eca4e
Nonga001 <135269562+Nonga001@users.noreply.github.com>
2023-09-27 16:57:39 +0300

    Add files via upload

--- results.java
+++ results.java
@@ -2,84 +2,75 @@ package semester;
 import java.util.Scanner;
 
 public class results {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int[] rollNo = new int[4];
+        String[] studName = new String[4];
+        int[] marks1 = new int[4];
+        int[] marks2 = new int[4];
+        int[] marks3 = new int[4];
+        int[] totals = new int[4];
+        String[] result = new String[4]; // Keep "RESULT" as a string
+        double[] average = new double[4];
+        String[] grades = new String[4];
 
-    public static void main(String[] args)
-        {
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Enter details for Student " + (i + 1));
             
-            final String name="Nonga Shaldon", Roll_No="C026-01-0975/2022", Semester="2.2";
-
-            String unitCode[] = new String[4];
-            String unitName[] = new String[4];
-            int score[] = new int[4];
-
-            double total = 0, average;
-            char grade;
-
-            Scanner input = new Scanner(System.in);
-            for(int i=0;i<score.length;i++)
-            {
-                System.out.print("Enter your Unit name"+(i+1)+"\nName: ");
-                unitName[i] = input.next();
-
-                System.out.print("Enter your Unit Code"+(i+1)+"\nCode: ");
-                unitCode[i]= input.next();
-
-                System.out.print("Enter your Score "+(i+1)+"\nScore: " );
-                score[i] = input.nextInt();
-            }
-
-            for(int mark = 0;mark<score.length; mark++)
-            {
-                total+=score[mark];
-            }
-
-            average = total/4;
-
-            if(average >= 70 && average <= 100)
-                grade='A';
-            else if(average >= 60)
-                grade ='B';
-            else if(average >= 50)
-                grade='C';
-            else if(average >=40)
-                grade = 'D';
-            else if(average >=0)
-                grade = 'F';
-            else
-                grade = '-';            
-
-            System.out.println("\t\t\t Department of Computer Science\n\t\t\t\tEnd Semester Results");
-            System.out.println("------------------------------------------------------------------------------");
-            System.out.println("|Name:- " +name+"\t|Roll_No:- "+Roll_No+"\tSemester:- "+Semester);
-            System.out.println("..............................................................................");
-            System.out.println("|Unit Code\t\t|Unit Name:\t\t\tScore");
-            System.out.println("------------------------------------------------------------------------------");
+            // Prompt for roll number as a string and then parse it into an integer
+            System.out.print("Enter Roll Number: ");
+            String rollNoStr = scanner.nextLine();
+            rollNo[i] = Integer.parseInt(rollNoStr);
             
-            for(int i=0;i<score.length;i++)
-            {
-                System.out.println("|"+unitCode[i]+"\t\t|"+unitName[i]+"\t\t\t|"+score[i]+"\n");
+            System.out.print("Enter Student's name: ");
+            studName[i] = scanner.nextLine();
+            
+            System.out.print("Enter marks for subject 1: ");
+            marks1[i] = scanner.nextInt();
+            
+            System.out.print("Enter marks for subject 2: ");
+            marks2[i] = scanner.nextInt();
+            
+            System.out.print("Enter marks for subject 3: ");
+            marks3[i] = scanner.nextInt();
+            
+            scanner.nextLine(); // Consume the newline character left in the buffer
+            
+            System.out.print("Enter RESULT: "); // Prompt for "RESULT"
+            result[i] = scanner.nextLine(); // Store "RESULT" as a string
+            
+            totals[i] = marks1[i] + marks2[i] + marks3[i];
+            average[i] = (double) totals[i] / 3;
+            if (average[i] >= 80 && average[i] <= 100) {
+                grades[i] = "A";
+            } else if (average[i] >= 75 && average[i] < 80) {
+                grades[i] = "B+";
+            } else if (average[i] >= 65 && average[i] < 75) {
+                grades[i] = "B";
+            } else if (average[i] >= 55 && average[i] < 65) {
+                grades[i] = "C+";
+            } else if (average[i] >= 50 && average[i] < 55) {
+                grades[i] = "C";
+            } else if (average[i] >= 40 && average[i] < 50) {
+                grades[i] = "D";
+            } else if (average[i] >= 0 && average[i] < 40) {
+                grades[i] = "F";
+            } else {
+                grades[i] = "INVALID";
             }
-
-            System.out.println("|Total\t\t\t\t\t\t\t|"+((int)total));
-            System.out.println("------------------------------------------------------------------------------");
-           
-            System.out.println("|Average\t\t\t\t\t\t|"+average);
-            System.out.println("------------------------------------------------------------------------------");
-           
-            System.out.println("|Grade\t\t\t\t\t\t\t|"+grade);
-            System.out.println("------------------------------------------------------------------------------");
-           
-            System.out.println("\t\t\t\t\tRecommendation  |");
-            System.out.println("------------------------------------------------------------------------------");
-           
-            System.out.println("Grading Creiteria\t\tOverall Grade(A-D)\t Recommend Pass");
-            System.out.println("70-100\t\tA");
-            System.out.println("60-70\t\tB");
-            System.out.println("50-60\t\tC");
-            System.out.println("40-50\t\tD");
-            System.out.println("Below 40 \tF");
-            System.out.println("------------------------------------------------------------------------------");
-
-            input.close();
         }
+
+        System.out.println("***********************************************************************");
+        System.out.println("\t\t\tSTUDENT MARKLIST");
+        System.out.println("***********************************************************************");
+        System.out.printf("ROLL \tNAME \tMARK1 \tMARK2 \tMARK3 \tTOTAL \tRESULT \tAverage\tGRADE%n");
+        
+        for (int i = 0; i < 4; i++) {
+            System.out.printf("%d\t%s\t%d\t%d\t%d\t%d\t%s\t%.1f\t%s%n",
+                    rollNo[i], studName[i], marks1[i], marks2[i], marks3[i], totals[i], result[i], average[i], grades[i]);
+        }
+
+        scanner.close();
+    }
 }
